3e1e2b55badb9a6a5e8ad142ed57255e64a77e57
samsamisamsam <samuel.larouche.cote@gmail.com>
2024-09-25 22:50:43 -0400

    copied circleshape.py (sprites subclass)

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     dt = 0
 
@@ -20,7 +18,7 @@ def main():
                 return
         
         # Fills the screen with black
-        pygame.Surface.fill(screen, 000000)
+        screen.fill(000000)
         # Updates screen's content
         pygame.display.flip()
 
