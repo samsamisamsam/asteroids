80d0fec11678d9326c89b9f5b1de7df0132a7b06
samsamisamsam <samuel.larouche.cote@gmail.com>
2024-09-25 22:42:39 -0400

    fps caped 60

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         # Check if user has clicked exit window button, then returns (which auto calls display.quit())
         for event in pygame.event.get():
@@ -20,6 +23,9 @@ def main():
         pygame.Surface.fill(screen, 000000)
         # Updates screen's content
         pygame.display.flip()
+
+        # Limits game to 60 fps and stores the time passed since last call in seconds
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
