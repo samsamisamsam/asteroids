3bd2de1ef03c415b52e7f5a803558cc1831b4e37
samsamisamsam <samuel.larouche.cote@gmail.com>
2024-09-25 23:32:15 -0400

    player moves

--- player.py
+++ player.py
@@ -22,6 +22,11 @@ class Player(CircleShape):
         pygame.draw.polygon(screen, 0xffffff, self.triangle(), 2)
 
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
@@ -33,3 +38,7 @@ class Player(CircleShape):
             self.rotate(-dt)    
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
